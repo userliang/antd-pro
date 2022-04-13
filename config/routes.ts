b8626290074e4d9339f04ad2115257845116b7f8
liangtaojkl123 <547508229@qq.com>
2022-04-13 14:18:09 +0800

    feat: i18n-remove

--- config/routes.ts
+++ config/routes.ts
@@ -1,58 +1,24 @@
-﻿export default [
+export default [
   {
     path: '/user',
     layout: false,
     routes: [
-      {
-        path: '/user',
-        routes: [
-          {
-            name: 'login',
-            path: '/user/login',
-            component: './user/Login',
-          },
-        ],
-      },
-      {
-        component: './404',
-      },
+      { path: '/user', routes: [{ path: '/user/login', component: './user/Login' }] },
+      { component: './404' },
     ],
   },
-  {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
+  { path: '/welcome', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
-    name: 'admin',
     icon: 'crown',
     access: 'canAdmin',
     component: './Admin',
     routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
+      { path: '/admin/sub-page', icon: 'smile', component: './Welcome' },
+      { component: './404' },
     ],
   },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
-  {
-    path: '/',
-    redirect: '/welcome',
-  },
-  {
-    component: './404',
-  },
+  { icon: 'table', path: '/list', component: './TableList' },
+  { path: '/', redirect: '/welcome' },
+  { component: './404' },
 ];
